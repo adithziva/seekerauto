2c89f9ba3e5faf04a91f18124856b2238d19577e
blvck <63218178+adithziva@users.noreply.github.com>
2021-03-20 23:56:35 +0530

    Add files via upload

---
+++ seekerauto.sh
@@ -0,0 +1,25 @@
+cowsay -f dragon Mr_blvck_ | lolcat
+figlet -f standard BLVCK | lolcat
+echo -e "\e[96m"
+echo " "
+echo "                      "⚡SINCE:2004⚡
+echo " "
+echo "";
+echo -e '\e[0;35m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[00m';
+echo -e ' [#] WE CAN BREAK SECURITY NOT HEART [#] \033[0m';
+echo -e '\e[0;35m+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[00m';
+
+echo -e '\e[0;35m         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[00m';
+echo -e '\034                 [#]  INSTAGRAM : MR_blvck_ ';
+echo -e '\e[0;35m         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\e[00m';
+figlet -f slant "Starting..."| lolcat
+
+
+git clone https://github.com/thewhiteh4t/seeker
+ls
+cd seeker
+ls
+pkg install python php -y
+bash install.sh
+python3 seeker.py -t manual
+
